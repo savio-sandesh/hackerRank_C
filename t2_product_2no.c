566a63a2d497d350da239d2dad27ec0be1fdc239
savio-sandesh <127104160+savio-sandesh@users.noreply.github.com>
2023-05-18 21:50:18 +0530

    Create t2_product_2no.c

---
+++ t2_product_2no.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int x,y,p;
+    scanf("%d %d",&x,&y);
+    p=x*y;
+    printf("%d",p);
+    return 0;
+}
